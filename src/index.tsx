df786d7328ea722b35b2540eb16ef249f3315d2d
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2018-12-27 18:40:21 +0900

    Add redux-observable

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,11 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 import { Provider } from "react-redux"
-import { createStore } from "redux"
 
 import { AppContainer } from "./App"
-import { items } from "./modules/Item"
+import configureStore from "./configureStore"
 
-declare var window: any
-
-const store = createStore(
-  items,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+const store = configureStore()
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ src/modules/index.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux"
+// import { combineEpics } from "redux-observable"
+
+import { items } from "./Item"
+
+// export const rootReducer = combineReducers({ items })
+export const rootReducer = items
+// export const rootEpics = combineEpics()
