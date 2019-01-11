22228678821ebd52bb00f1af064820d15b7027ba
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2019-01-11 17:35:46 +0900

    雑に改行を適用

--- src/components/MemoItem.tsx
+++ src/components/MemoItem.tsx
@@ -9,17 +9,19 @@ interface IMemoProps {
 
 export const MemoItem = (props: IMemoProps) => {
   const d = new Date(props.memo.created_at)
-  const body = props.memo.text
-
   const style = {
     fontSize: "80%",
   }
+
+  const re = /\n/gi
+  const body = props.memo.text.replace(re, "<br />")
+  const createMarkup = () => ({ __html: body })
 
   return (
     <div className="card">
       <div className="card-content">
         <div className="content">
-          {body}
+          <span dangerouslySetInnerHTML={createMarkup()} />
           <br />
           <span style={style}>{formatYMDHms(d)}</span>
         </div>
